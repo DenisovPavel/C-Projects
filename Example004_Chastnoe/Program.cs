abbffd86e2510444b765e9064460390ba0e44e40
DenisovPavel <Waste11@mail.ru>
2022-11-20 01:05:15 +0300

    add 004 and practiced

---
+++ Example004_Chastnoe/Program.cs
@@ -0,0 +1,9 @@
+﻿double numberA = 13;
+double numberB = 5;
+double div = numberA / numberB;
+Console.WriteLine(div);
+
+double numberC = 4;
+double numberD = 9;
+double div2 = numberC % numberD;
+Console.WriteLine(div2);
