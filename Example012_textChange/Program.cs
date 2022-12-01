e743832019e57841cc4de64f47229a602a795cac
DenisovPavel <Waste11@mail.ru>
2022-12-01 19:54:11 +0300

    lection3

---
+++ Example012_textChange/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+string text = "- Я думаю, – сказал князь, улыбаясь, – что, "
+ + " ежели бы вас послали вместо нашего милого Винценгероде,"
+ + " вы бы взяли приступом согласие прусского короля."
+ + " Вы так красноречивы. Вы дадите мне чаю? ";
+
+string Replace(string text, char oldValue, char newValue)
+{
+    int lenght = text.Length;
+    string result = String.Empty;
+
+    for (int i = 0; i < lenght; i++)
+    {
+
+        if (text[i] == oldValue)
+            result = result + $"{newValue}";
+
+        else result = result + $"{text[i]}";
+
+    }
+
+    return result;
+}
+string newtext = Replace(text,' ','|');
+Console.WriteLine(newtext);
+Console.WriteLine();
+string newtext1 = Replace(newtext,'к','К');
+Console.WriteLine(newtext1);
+Console.WriteLine();
+string newtext2 = Replace(newtext1,'|','_');
+Console.WriteLine(newtext2);
---
+++ Example013_Bubblesort/Program.cs
@@ -0,0 +1,34 @@
+﻿
+int[] arr = { 8, 7, 4, 6, 5, 1, 3, 2, 4 };
+
+void PrintArray(int[] array)
+{
+
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+
+void SelectionArray(int[] array)
+{
+
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minelement = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minelement]);
+            minelement = j;
+        }
+        int temp = array[i];
+        array[i] = array[minelement];
+        array[minelement] = temp;
+    }
+}
+PrintArray(arr);
+SelectionArray(arr);
+PrintArray(arr);
